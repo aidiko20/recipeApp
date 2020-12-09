90c221612f34b5fc94665f293e29dd685b848ac2
Aida Yrysbekova <aidiko20@gmail.com>
2020-12-08 21:34:18 -0500

    added some logic

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
- async function getRandomMeal() {
-    const randomMeal = fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+getRandomMeal();
+
+ 
+async function getRandomMeal() {
+    const randomMeal = await await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    const randomMeal = await resp;
+
+    console.log(randomMeal);
 }
 
 function getMealById(id) {
-    const meal = fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' +id)
+    const meal = await fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' +id)
 }
 
 function getMealsBySearch(term) {
-    const meals = fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' +term)
+    const meals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' +term)
 
 }
