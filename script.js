0d77b41192870fc653c7aa01291b939b3705e65f
Aida Yrysbekova <aidiko20@gmail.com>
2020-12-08 22:04:35 -0500

    fixed an error

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ async function getRandomMeal() {
     const randomMeal = respData.meals[0];
 
     addMeal(randomMeal, true);
-}
+};
 
 async function getMealById(id) {
     const meal = await fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id)
-}
+};
 
 async function getMealsBySearch(term) {
     const meals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + term)
 
-}
+};
 
-addMeal(mealData, random = false) {
+function addMeal(mealData, random = false) {
     const meal = document.getElementById('div');
     meal.classList.add('meal');
 
@@ -37,4 +37,6 @@ addMeal(mealData, random = false) {
         </div>
     </div>
     `;
+
+    meals.appendChild(meal);
 }
