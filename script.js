e3c97e400e3eadd186cad35c45076cc2c092361a
Aida Yrysbekova <aidiko20@gmail.com>
2020-12-12 21:48:26 -0500

    finished project final push

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const favoriteContainer = document.getElementById("fav-meals");
 const searchTerm = document.getElementById("search-term");
 const searchBtn = document.getElementById("search");
 const mealPopup = document.getElementById("meal-popup");
-const closePopupBtn = document.getElementById("close-popup");
+const mealInfoEl = document.getElementById("meal-info")
+const popupCloseBtn = document.getElementById("close-popup");
 
 getRandomMeal();
 fetchFavMeals();
@@ -43,24 +44,29 @@ async function getMealsBySearch(term) {
 
 function addMeal(mealData, random = false) {
     console.log(mealData);
+
     const meal = document.createElement("div");
     meal.classList.add("meal");
 
-    meal.innerHTML = `            
+    meal.innerHTML = `
         <div class="meal-header">
-        ${random
-            ? `
-        <span class="random">Random Recipe</span>`
-            : " "
-        }
-            <img src="${mealData.strMealThumb}" 
-            alt="${mealData.strMeal}">
+            ${
+                random
+                    ? `
+            <span class="random"> Random Recipe </span>`
+                    : ""
+            }
+            <img
+                src="${mealData.strMealThumb}"
+                alt="${mealData.strMeal}"
+            />
         </div>
         <div class="meal-body">
             <h4>${mealData.strMeal}</h4>
-            <button class="fav-btn"><i class="fas fa-heart"></i></button>
+            <button class="fav-btn">
+                <i class="fas fa-heart"></i>
+            </button>
         </div>
-    </div>
     `;
 
     const btn = meal.querySelector(".meal-body .fav-btn");
@@ -77,7 +83,11 @@ function addMeal(mealData, random = false) {
         fetchFavMeals();
     });
 
-    meals.appendChild(meal);
+    meal.addEventListener("click", () => {
+        showMealInfo(mealData);
+    });
+
+    mealsEl.appendChild(meal);
 }
 
 function addMealLS(mealId) {
@@ -102,12 +112,12 @@ function getMealsLS() {
 }
 
 async function fetchFavMeals() {
+    // clean the container
     favoriteContainer.innerHTML = "";
 
     const mealIds = getMealsLS();
-    const meals = [];
 
-    for (let i = 0; i < mealIds.lenght; i++) {
+    for (let i = 0; i < mealIds.length; i++) {
         const mealId = mealIds[i];
         meal = await getMealById(mealId);
 
@@ -117,18 +127,77 @@ async function fetchFavMeals() {
 
 function addMealFav(mealData) {
     const favMeal = document.createElement("li");
-    favMeal.innerHTML = `            
-    <li><img src="${mealData.strMealThumb}" alt="${mealData.strMeal}"><span>${mealData.strMeal}</span></li>
-    <button class="clear"><i class="fas fa-window-close"></button>
-      `;
 
-    const btn = favMeal.querySelector('clear');
-    btn.addEventListener('click', () => {
+    favMeal.innerHTML = `
+        <img
+            src="${mealData.strMealThumb}"
+            alt="${mealData.strMeal}"
+        /><span>${mealData.strMeal}</span>
+        <button class="clear"><i class="fas fa-window-close"></i></button>
+    `;
+
+    const btn = favMeal.querySelector(".clear");
+
+    btn.addEventListener("click", () => {
         removeMealLS(mealData.idMeal);
 
         fetchFavMeals();
     });
+
+    favMeal.addEventListener("click", () => {
+        showMealInfo(mealData);
+    });
+
     favoriteContainer.appendChild(favMeal);
+}
+
+function showMealInfo(mealData) {
+    // clean it up
+    mealInfoEl.innerHTML = "";
+
+    // update the Meal info
+    const mealEl = document.createElement("div");
+
+    const ingredients = [];
+
+    // get ingredients and measures
+    for (let i = 1; i <= 20; i++) {
+        if (mealData["strIngredient" + i]) {
+            ingredients.push(
+                `${mealData["strIngredient" + i]} - ${
+                    mealData["strMeasure" + i]
+                }`
+            );
+        } else {
+            break;
+        }
+    }
+
+    mealEl.innerHTML = `
+        <h1>${mealData.strMeal}</h1>
+        <img
+            src="${mealData.strMealThumb}"
+            alt="${mealData.strMeal}"
+        />
+        <p>
+        ${mealData.strInstructions}
+        </p>
+        <h3>Ingredients:</h3>
+        <ul>
+            ${ingredients
+                .map(
+                    (ing) => `
+            <li>${ing}</li>
+            `
+                )
+                .join("")}
+        </ul>
+    `;
+
+    mealInfoEl.appendChild(mealEl);
+
+    // show the popup
+    mealPopup.classList.remove("hidden");
 }
 
 searchBtn.addEventListener('click', async () => {
@@ -144,6 +213,6 @@ searchBtn.addEventListener('click', async () => {
     }
 });
 
-closePopupBtn.addEventListener('click', () => {
+popupCloseBtn.addEventListener('click', () => {
     mealPopup.classList.add('hidden');
-})
+});
